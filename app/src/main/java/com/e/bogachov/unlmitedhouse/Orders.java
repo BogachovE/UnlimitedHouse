9adb664d4d1b21f1b6521e316b52df6aa9300fc9
bogachov <jackwhite33@icloud.com>
2016-08-26 14:19:13 +0300

    Ready add service UI and list try

--- app/src/main/java/com/e/bogachov/unlmitedhouse/Orders.java
+++ app/src/main/java/com/e/bogachov/unlmitedhouse/Orders.java
@@ -1,13 +1,31 @@
 package com.e.bogachov.unlmitedhouse;
 
+import android.app.ListActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
-public class Orders extends AppCompatActivity {
+public class Orders extends ListActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.orders);
-    }
+
+        String[] names = { "Иван", "Марья", "Петр", "Антон", "Даша", "Борис",
+                "Костя", "Игорь", "Анна", "Денис", "Андрей" };
+
+        /** Called when the activity is first created. */
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.orders);
+
+            // находим список
+            ListView lvMain = (ListView) findViewById(R.id.lvMain);
+
+            // создаем адаптер
+            ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                    android.R.layout.simple_list_item_1, names);
+
+            // присваиваем адаптер списку
+            lvMain.setAdapter(adapter);
+
+        }
 }
