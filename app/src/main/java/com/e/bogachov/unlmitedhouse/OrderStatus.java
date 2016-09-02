bf8f40c1ab4db842a72368b3201c58f2400ea334
bogachov <jackwhite33@icloud.com>
2016-09-02 22:59:52 +0300

    OrderStatusStart

---
+++ app/src/main/java/com/e/bogachov/unlmitedhouse/OrderStatus.java
@@ -0,0 +1,13 @@
+package com.e.bogachov.unlmitedhouse;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class OrderStatus extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.order_status);
+    }
+}
